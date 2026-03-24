69d06a53488ef9c68b30df15042ed0a061417449
Yuyu Seryi <stos2583@yandex.ru>
2026-03-24 14:36:31 +0300

    log добавил

--- src/main/java/ru/yuyu/myshoppayment1/PaymentService.java
+++ src/main/java/ru/yuyu/myshoppayment1/PaymentService.java
@@ -1,16 +1,18 @@
 package ru.yuyu.myshoppayment1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
+@Slf4j
 public class PaymentService {
     public ProductResponseDto pay(ProductRequestDto productRequestDto) {
         String result = "payment_error";
         if (productRequestDto.paymentMethod().equals("CARD"))
             result = "success";
-
+        log.info("Результат оплаты: {}", result);
        return ProductResponseDto.builder()
                 .name(productRequestDto.name())
                 .PaymentId(ThreadLocalRandom.current().nextLong(100))
